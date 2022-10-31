e6a34949e9ab96b1abb775b4d1ff6b69b7507110
Chris Mittendorf <u15092462@tuks.co.za>
2022-10-31 09:34:57 +0200

    Vehicle Template and Concrete classes
    Vehicle template to be used by factory and concrete classes; Tank, Plane and Ship

---
+++ Vehicle.cpp
@@ -0,0 +1,82 @@
+//
+// Created by chris on 2022/10/31.
+//
+
+#include "Vehicle.h"
+#include <iostream>
+
+Vehicle::Vehicle() {
+    type = "War Vehicle";
+    state = "Under Construction";
+    armour = 50;
+    damage = 0;
+}
+
+Vehicle::Vehicle(string t, string s) {
+    type = t;
+    state = s;
+    armour = 200;
+    damage = 75;
+}
+
+void Vehicle::clone() {
+    //apply prototype here
+}
+
+string Vehicle::getType() {
+    return this->type;
+}
+
+Vehicle::~Vehicle(){
+    delete this;
+}
+
+Plane::Plane() : Vehicle(){
+}
+
+Plane::Plane(string t, string s) : Vehicle(t, s){
+    armour = 75;
+    bomb = 150;
+    strafe = 50;
+}
+
+int Plane::bombingRun() {
+    return this->bomb;
+}
+
+int Plane::dogFight() {
+    return  this->strafe;
+}
+
+Tank::Tank() : Vehicle() {
+}
+
+Tank::Tank(string t,string s) : Vehicle(t, s){
+    armour = 150;
+    damage = 100;
+}
+
+int Tank::fire() {
+    return this->damage;
+}
+
+int Tank::returnFire() {
+    return this->damage;
+}
+
+Ship::Ship() : Vehicle(){
+}
+
+Ship::Ship(string t, string s) : Vehicle(t, s) {
+    armour = 300;
+    shell = 250;
+    canon = 150;
+}
+
+int Ship::shellFire() {
+    return this->shell;
+}
+
+int Ship::counter() {
+    return this->canon;
+}
