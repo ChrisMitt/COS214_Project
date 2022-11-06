38cc10fede262669df40b6078e6ddb313bc44023
shiluvelomakhubele <u19086352@tuks.co.za>
2022-11-06 14:13:25 +0200

    Update State.h

--- System/State.h
+++ System/State.h
@@ -6,26 +6,58 @@
 #define STATE_H
 #include <iostream>
 using namespace std;
-
+/**
+ * @brief a State class.
+ * 
+ */
 class State {
   public:
+  /**
+   * @brief Get the State String object
+   * 
+   * @return string 
+   */
     string getStateString() {return state;};
   protected:
+  /**
+   * @brief a protected object
+   * a string object
+   */
     string state;
 };
 
+/**
+ * @brief a peacestate class
+ * 
+ */
 class peaceState : public State {
   public:
     peaceState() {state = "Peace";};
 };
 
+/**
+ * @brief a warstate class
+ * 
+ */
 class warState : public State {
  public:
+ /**
+  * @brief Construct a new war State object
+  * 
+  */
     warState() {state = "War";};
 };
 
+/**
+ * @brief a egotiatingState class
+ * 
+ */
 class negotiatingState : public State {
   public:
+  /**
+   * @brief Construct a new negotiating State object
+   * 
+   */
     negotiatingState() {state = "Negotiating";};
 };
 
