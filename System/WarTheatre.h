31dbaa4ae598a6506ec381bfdd082c8501ed4a76
shiluvelomakhubele <u19086352@tuks.co.za>
2022-11-06 14:47:19 +0200

    Update Wartheatre.h

--- System/WarTheatre.h
+++ System/WarTheatre.h
@@ -15,11 +15,29 @@
 #include "Squad.h"
 using namespace std;
 
+
+/**
+ * @brief a Wartheatre calss. this where the war takes place
+ * 
+ */
+
 class WarTheatre
 {
 private:
+/**
+ * @brief a list of Alliances 
+ * 
+ */
     list<Alliance*> allAlliances;
+    /**
+     * @brief A squad pointer object
+     * 
+     */
     Squad* defenders;
+     /**
+     * @brief A squad pointer object
+     * 
+     */
     Squad* attackers;
 
     //used by fight methods
@@ -27,16 +45,71 @@ private:
     list<Tank*> allTanks;//combined attacker and defender tanks
 
 public:
+/**
+ * @brief  Construct a new War Theatre object
+ * 
+ */
     WarTheatre(/* args */);
+    /**
+     * @brief Construct a new War Theatre object
+     * 
+     * @param as 
+     */
     WarTheatre(list<Alliance*> as);
+    /**
+     * @brief Destroy the War Theatre object
+     * 
+     */
     ~WarTheatre();
+    /**
+     * @brief a normal member
+     * 
+     */
     void fillLists();//allSoldiers, allTanks etc..
+    /**
+     * @brief a normal member
+     * 
+     * @param d 
+     * @param a 
+     * @param area 
+     * @param country 
+     */
     void battle( list<CountryObserver*> d, list<CountryObserver*> a, string area, string country );
+    /**
+     * @brief a normal member
+     * 
+     */
     void fight();
+    /**
+     * @brief a normal member
+     * 
+     */
     void soldiersFight();
+    /**
+     * @brief a normal member
+     * 
+     */
     void tanksFight();
+    /**
+     * @brief a normal member
+     * 
+     */
     void medicsHeal();
+    /**
+     * @brief a normal member
+     * 
+     */
     void mechanicsRepair();
+    /**
+     * @brief a normal member
+     * 
+     * @param damageDealer 
+     * @param enemySquad 
+     */
     void tankAttack(Tank* damageDealer, Squad* enemySquad);
+    /**
+     * @brief a normal memeber
+     * 
+     */
     void removeDeadUnits();
 };
