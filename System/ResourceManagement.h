60da7720536d0158918cf54afefe0da0ff195a78
shiluvelomakhubele <u19086352@tuks.co.za>
2022-11-07 22:38:07 +0200

    documentation

--- System/ResourceManagement.h
+++ System/ResourceManagement.h
@@ -10,20 +10,56 @@
 #include "CountryObserver.h"
 #include "Country.h"
 
-
+/**
+ * @brief a Resource  Management class
+ * 
+ */
 class ResourceManagement {
 public:
+/**
+ * @brief Construct a new Resource Management object
+ * 
+ */
     ResourceManagement();
+    /**
+     * @brief a normal member
+     * 
+     * @param ResourceManagement*
+     */
     void add(ResourceManagement*);
+    /**
+     * @brief Create a Upgraded Unit object
+     * 
+     * @return int 
+     */
     virtual int createUpgradedUnit(int, Country*);
 
 private:
+/**
+ * @brief ResourceManagement pointer object
+ * 
+ */
     ResourceManagement* next;
 };
 
+/**
+ * @brief a UnitProducer class
+ * 
+ */
 class UnitProducer : public ResourceManagement{
 public:
+/**
+ * @brief Construct a new Unit Producer object
+ * @param int
+ * @param int
+ */
     UnitProducer(int, int);
+    /**
+     * @brief Create a Upgraded Unit object
+     * @param int
+     * @param Country*
+     * @return int 
+     */
     int createUpgradedUnit(int, Country*);
 
 private:
